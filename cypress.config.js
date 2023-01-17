3028689f97fe1d352680cacfd448070155ea58d9
cezarfelipe10 <qacompassesfera@gmail.com>
2023-01-17 16:13:45 -0300

    Configure cypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
     baseUrl: "https://notes-serverless-app.com",
     defaultCommandTimeout: 90000,
     "chromeWebSecurity": false,
+    "projectId": "6jpe8q",
+    
 
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
